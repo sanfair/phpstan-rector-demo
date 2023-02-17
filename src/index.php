97f5c44ae7d03a121f0245cd5586ebebb6eb06f3
Yauheni Lukyantsau <pro100.freeman@gmail.com>
2023-02-17 11:13:17 +0100

    7.4 -x-> 8.0

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+
+// Comparison checks.
+
+if (0 == "foo") {
+    $output = "0 is foo";
+}
+else {
+    $output = "0 is not foo";
+}
+echo $output, "\n";
+
+
+// Each.
+$fruit = array('a' => 'apple', 'b' => 'banana', 'c' => 'cranberry');
+
+reset($fruit);
+while (list($key, $val) = each($fruit)) {
+    echo "$key => $val";
+}
+
+//  array_key_exists() with objects.
+
+$object = new stdClass();
+$object->key = 'value';
+
+if (array_key_exists('key', $object)) {
+    echo $object->key, "\n";
+}

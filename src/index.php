a029e6601a995c11bd99f9e72a92d3b2df024a4b
Yauheni Lukyantsau <pro100.freeman@gmail.com>
2023-03-06 13:30:35 +0100

    Update demo script

--- src/index.php
+++ src/index.php
@@ -1,44 +1,101 @@
 <?php
 
-// Comparison checks.
-
-/** @var string $string */
-$string = "foo";
-$zero = 0;
-
-if ($zero == $string) {
-    $output = "0 is foo";
+// <---- 8.0 ---->
+/** Str contains */
+function testSubstring(string $string, string $subString): bool
+{
+    return strpos($string, $subString) !== false;
 }
-else {
-    $output = "0 is not foo";
+testSubstring('Hi, my name is', 'name');
+
+/** Str Starts With */
+function testStartsWith(string $string, string $subString): bool
+{
+    return substr($string, 0, strlen($subString)) === $subString;
 }
-echo $output, "\n";
+testStartsWith('My name is', 'My');
 
-
-// Each.
-$fruit = array('a' => 'apple', 'b' => 'banana', 'c' => 'cranberry');
-
-reset($fruit);
-while (list($key, $val) = each($fruit)) {
-    echo "$key => $val";
+/** Str Ends With */
+function testEndsWith(string $string, string $subString): bool
+{
+    return substr($string, -strlen($subString)) === $subString;
 }
+testEndsWith('My name is', 'Slim Shady');
 
-//  array_key_exists() with objects.
+/** Set State warning */
+class SetStateClass
+{
+    public function __set_state($properties) {
 
-$object = new stdClass();
-$object->key = 'value';
-
-if (array_key_exists('key', $object)) {
-    echo $object->key, "\n";
-}
-
-class Old implements Serializable {
-
-    public function serialize()
-    {
-    }
-
-    public function unserialize($data)
-    {
     }
 }
+
+/** Final Private Warning */
+class FinalPrivateClass
+{
+    final private function getter() {
+        return $this;
+    }
+}
+
+/** Deprecate required parameters after optional  */
+function testOptionalBeforeRequired($optional = 1, $required)
+{
+}
+class OptionalBeforeRequiredClass
+{
+    public function method($optional = 1, $required)
+    {
+    }
+}
+
+// <---- 7.4 ---->
+/** Deprecate is_real and (real) casting */
+(real) 0.42069;
+
+is_real(3.14159265359);
+
+/** Deprecate array_key_exists on objects */
+$object = new \stdClass();
+$object->value = 'something';
+array_key_exists('value', $object);
+
+/** Change mb_strrpos() encoding argument position */
+mb_strrpos('abcd', 'abc', 'UTF-8');
+
+/** Array and string offset access using curly braces */
+$string = 'test';
+$string{0};
+$array = ['test'];
+$array{0};
+
+/** Nested ternary operators without explicit parentheses */
+$a = 1;
+$b = 2;
+$value = $a ? $b : $a ?: null;
+
+// <---- 7.3 ---->
+/** Case-Insensitive Constants */
+define('FOO', 42, true);
+
+/** String search functions with integer needle */
+$str = "There are 97 apples";
+strpos($str, "97"); // int(10)
+strpos($str, 97);   // int(6) -> int(10)
+
+/** PCRE2 migration */
+preg_match("#[\w-()]#", 'some text');
+
+// <---- 7.2 ---->
+/** Each */
+$callbacks = ['test', 'other'];
+while (list($key, $callback) = each($callbacks)) {
+    // ...
+}
+
+/** Assert */
+assert('true === true');
+
+assert("true === true");
+
+echo "Finished\n";
